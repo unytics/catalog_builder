7ae440833ef02e00e3b6ac5fbca30b4ac9c074db
unytics <paul.marcombes@unytics.io>
2024-03-08 14:20:54 +0000

    deployment on github pages

--- catalog_builder/cli.py
+++ catalog_builder/cli.py
@@ -2,6 +2,7 @@ import os
 
 import click
 from click_help_colors import HelpColorsGroup
+import yaml
 
 from .catalogs import Catalog, CatalogException
 from .utils import handle_error, print_info, download_github_folder, exec
@@ -58,3 +59,45 @@ def serve(catalog_name):
         raise CatalogException(f'`{catalog.folder}/site` folder does not exist. Please build it with `build` command')
     print_info(f'Serving website. Open this url in your browser --> http://localhost:8000 !')
     exec(f'python -m http.server --directory {catalog.folder}/site')
+
+
+@cli.group()
+def deploy():
+    '''
+    Deploy documentation website
+    '''
+    pass
+
+
+@deploy.command()
+@click.argument('catalog_name')
+def github_pages(catalog_name):
+    '''
+    Deploy documentation website on GitHub pages
+    '''
+    catalog = Catalog(catalog_name)
+    config_file = f'catalogs/{catalog_name}/mkdocs.yml'
+    if not os.path.isfile(config_file):
+        raise CatalogException(f'Missing config file {config_file}')
+    print_info(f'Deploying site to GitHub pages')
+    exec(f'mkdocs gh-deploy --config-file {config_file} --force')
+
+
+# @deploy.command()
+# @click.argument('catalog_name')
+# def gcs(catalog_name):
+#     '''
+#     Deploy documentation website on a Google Cloud Storage bucket
+#     '''
+#     catalog = Catalog(catalog_name)
+#     config_file = f'catalogs/{catalog_name}/mkdocs.yml'
+#     if not os.path.isdir(f'{catalog.folder}/site'):
+#         raise CatalogException(f'`{catalog.folder}/site` folder does not exist. Please build it with `build` command')
+#     if not os.path.isfile(config_file):
+#         raise CatalogException(f'Missing config file {config_file}')
+#     config = yaml.safe_load(open(config_file, encoding='utf-8').read())
+#     breakpoint()
+#     print_info(f'Deploying site to GitHub pages')
+#     exec(f'mkdocs gh-deploy --config-file {config_file} --force')
+
+#     gcloud storage cp catalogs/simple/site/pypi/distribution_metadata/index.html  gs://catalogs.unytics.io/bigquery_public_data/pypi/distribution_metadata/index.html
